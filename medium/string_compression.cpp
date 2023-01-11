60db0f5e12b2a72fba2cf9cd1295c2b625bb5d16
Alexander <kozhukhovmib@gmail.com>
2023-01-11 18:11:55 +0300

    compression of string O(n)

---
+++ medium/string_compression.cpp
@@ -0,0 +1,58 @@
+/*
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+    If the group's length is 1, append the character to s.
+    Otherwise, append the character followed by the group's length.
+
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        string compressed;
+        char to_remember = chars[0];
+        int counter = 1;
+        for (auto i = 1; i < chars.size(); ++i) {
+            if(chars[i] == to_remember) {
+                ++counter;
+            } else {
+                if(counter == 1) {
+                    compressed += to_remember;
+                } else {
+                    compressed += to_remember;
+                    compressed += to_string(counter);
+                }
+                counter = 1;
+                to_remember = chars[i];
+            }
+        }
+
+        if(counter > 1) {
+            compressed += to_remember;
+            compressed += to_string(counter);
+        } else {
+            compressed += to_remember;
+        }
+
+        chars.resize(compressed.size());
+        
+        for(int i = 0; i < compressed.size(); ++i) {
+            chars[i] = compressed[i];
+        }
+        
+        return chars.size();
+    }
+};
