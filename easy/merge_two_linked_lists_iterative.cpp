ca0efb23ebf6fd4cdf18828f905bf2b2d40a2cf7
Alexander <kozhukhovmib@gmail.com>
2023-01-12 12:01:00 +0300

    merge two linked lists with iterative approach

---
+++ easy/merge_two_linked_lists_iterative.cpp
@@ -0,0 +1,55 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+*/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1) {return list2;}
+        if(!list2) {return list1;}
+
+        ListNode* head; // заводим голову списка
+
+        // выбираем, откуда начинается последовательность элементов
+        if(list1->val > list2->val) {
+            head = list2;
+            list2 = list2->next;
+        } else {
+            head = list1;
+            list1 = list1->next;
+        }
+    
+        ListNode* curr = head; // указатель для того, чтобы двигаться по элементам
+        while (list1 && list2) {
+            if(list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        
+        if(!list1) { // если какой-то из списков остался
+            curr->next = list2;
+        } else {
+            curr->next = list1;
+        }
+
+        return head;
+    }
+};
